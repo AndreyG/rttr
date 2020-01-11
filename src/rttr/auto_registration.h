4709b83ada2fde34b9d225b15afb50fc18ad225a
Andrey Davydov <andrey.a.davydov@gmail.com>
2020-01-11 11:32:30 +0300

    simplify 'auto_enumeration'

--- src/rttr/auto_registration.h
+++ src/rttr/auto_registration.h
@@ -8,33 +8,18 @@ namespace rttr
 namespace meta = std::experimental::meta;
 
 template<typename Enum_Type>
-struct enumerator_t
-{
-    const char * name;
-    Enum_Type value;
-};
-
-template<typename Enum_Type>
 consteval auto get_enumerators()
 {
     constexpr meta::info enum_reflection = reflexpr(Enum_Type);
     constexpr auto members = meta::members_of(enum_reflection);
     constexpr auto size = std::distance(std::begin(members), std::end(members));
-    std::array<enumerator_t<Enum_Type>, size> result;
+    std::array<detail::enum_data<Enum_Type>, size> result;
     size_t i = 0;
     template for (constexpr auto enumerator : meta::members_of(enum_reflection))
     {
-        result[i].name = meta::name_of(enumerator);
-        result[i].value = valueof(enumerator);
-        ++i;
+        result[i++] = { meta::name_of(enumerator), valueof(enumerator) };
     };
     return result;
-}
-
-template<typename Enumeration, typename Enumerators, size_t... I>
-void apply(Enumeration enumeration, Enumerators const & enumerators, std::index_sequence<I...>)
-{
-    enumeration(value(enumerators[I].name, enumerators[I].value)...);
 }
 
 template<typename Enum_Type>
@@ -42,9 +27,8 @@ void auto_enumeration()
 {
     constexpr auto enumerators = get_enumerators<Enum_Type>();
     constexpr auto enum_name = meta::name_of(reflexpr(Enum_Type));
-    apply(registration::enumeration<Enum_Type>(enum_name),
-          enumerators,
-          std::make_index_sequence<enumerators.size()>());
+    std::apply(registration::enumeration<Enum_Type>(enum_name),
+               std::array /*create copy as temporary workaround for rttr bug*/ (enumerators));
 }
 
 template<typename MemberFuncPtr>
